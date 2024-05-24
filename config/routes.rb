9e7b118815f8502390540ac77935bb921f666bd6
carpioapaza <rodrigo25096@gmail.com>
2024-05-24 00:15:18 -0500

    : Add routes for user events and root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-        sessions: 'users/sessions'
-      }
+      sessions: 'users/sessions'
+    }
+  # scope module: "users" do
   namespace :users do
-    
+    resources :events
   end
+  get '/events', to: 'home#index_all', as: 'all_events'
+  root "home#index"
+
+    # Captura cualquier ruta no encontrada y muestra un error 404
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
