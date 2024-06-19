384a111abc342752db812a8174062758683f048c
carpioapaza <rodrigo25096@gmail.com>
2024-06-18 23:04:10 -0500

    add quantity wtihtin event capacity validation

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -23,4 +23,15 @@ class Ticket < ApplicationRecord
   validates :ticket_name, :quantity_available, :price, :currency, presence: true
   validates :quantity_available, numericality: { greater_than_or_equal_to: 0 }
   validates :price, numericality: { greater_than_or_equal_to: 0 }
+
+  validate :quantity_within_event_capacity
+
+  private
+
+  def quantity_within_event_capacity
+    total_tickets = event.tickets.where.not(id: id).sum(:quantity_available) + quantity_available
+    if total_tickets > event.capacity
+      errors.add(:base, "El total de entradas supera la capacidad del evento")
+    end
+  end
 end
